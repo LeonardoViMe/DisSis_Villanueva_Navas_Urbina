04b281fe7c25073a669eea00a4b7b332b84f2657
MiguelFU <106198273+MiguelFU@users.noreply.github.com>
2022-08-16 11:02:39 -0500

    Add files via upload

---
+++ Conteo Inverso.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+UA:Diseño de Sistemas
+
+Alumnos:Flores Urbina Miguel Angel
+        Diaz Navas Luis Angel
+        Villanueva Medina Leonardo
+Created on Tue Aug 16 10:09:28 2022
+
+@author: ICO_PelSw_14
+"""
+
+num=23
+print("Conteo inverso de edad 22")
+for i in range(1,num+1):
+    print(num-i)
