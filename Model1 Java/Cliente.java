7425419b1204ef7a48ae88823ad3be4ea3bd6fd0
MiguelFU <106198273+MiguelFU@users.noreply.github.com>
2022-08-28 20:21:26 -0500

    Add files via upload

---
+++ Model1 Java/Cliente.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Cliente {
+
+    /**
+     * Default constructor
+     */
+    public Cliente() {
+    }
+
+    /**
+     * 
+     */
+    private void ID del usuario;
+
+    /**
+     * 
+     */
+    private void Datos bancarios;
+
+    /**
+     * 
+     */
+    private void Direccion;
+
+    /**
+     * 
+     */
+    private void Telefono;
+
+    /**
+     * 
+     */
+    public Reservacion 1;
+
+
+
+    /**
+     * @param Telefono 
+     * @param online 
+     * @param presencial
+     */
+    public void Reservacion(void Telefono, void online, void presencial) {
+        // TODO implement here
+    }
+
+}
---
+++ Model1 Python/Reservacion.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Reservacion:
+    def __init__(self):
+        self.Modelo de auto = None
+        self.Marca = None
+        self.Matricula = None
+        self.Disponibilidad = None
+
+    def Alquiler(self, horas):
+        pass
+
+    def Alquiler(self, dias):
+        pass
+
+    def Entrega(self, fecha y hora):
+        pass
+
