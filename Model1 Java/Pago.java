41402302a9ad645d78eaeb98714e71f5b5c71887
MiguelFU <106198273+MiguelFU@users.noreply.github.com>
2022-08-28 20:24:46 -0500

    Add files via upload

---
+++ Model1 Java/Pago.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Pago {
+
+    /**
+     * Default constructor
+     */
+    public Pago() {
+    }
+
+    /**
+     * 
+     */
+    public void Prepago;
+
+
+    /**
+     * 
+     */
+    public void Puntos de venta() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Transferencia bancaria() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Efectivo() {
+        // TODO implement here
+    }
+
+}
